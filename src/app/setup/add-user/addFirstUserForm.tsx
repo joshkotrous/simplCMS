a8fc38420e10c315028a95c0942a01900a9feb5d
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2025-01-16 00:28:36 -0500

    Implemented redirect if first user already added

---
+++ src/app/setup/add-user/addFirstUserForm.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { createUserAction } from "@/app/actions/userActions";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function AddFirstUserForm() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  async function submit() {
+    setLoading(true);
+    toast.promise(createUserAction({ email: email }), {
+      loading: "Creating user...",
+      success: () => {
+        router.push("/login");
+        setLoading(false);
+        return "Successfully created user.";
+      },
+      error: () => {
+        setLoading(false);
+        return "Error creating user.";
+      },
+    });
+  }
+
+  return (
+    <Card>
+      <CardHeader className="text-center font-semibold">
+        Add Your First User
+      </CardHeader>
+      <CardContent className="space-y-4 w-96">
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email..."
+        />
+
+        <Button
+          disabled={email === "" || loading}
+          onClick={submit}
+          className="w-full"
+        >
+          Add User
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/setup/add-user/page.tsx
+++ src/app/setup/add-user/page.tsx
@@ -1,54 +1,13 @@
-"use client";
-import { createUserAction } from "@/app/actions/userActions";
-import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { toast } from "sonner";
+import { user } from "@/packages/core/src/user";
+import AddFirstUserForm from "./addFirstUserForm";
+import { redirect } from "next/navigation";
 
-export default function AddFirstUserPage() {
-  const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-  async function submit() {
-    setLoading(true);
-    toast.promise(createUserAction({ email: email }), {
-      loading: "Creating user...",
-      success: () => {
-        router.push("/login");
-        setLoading(false);
-        return "Successfully created user.";
-      },
-      error: () => {
-        setLoading(false);
-        return "Error creating user.";
-      },
-    });
-  }
-
+export default async function AddFirstUserPage() {
+  const users = await user.getAllUsers();
+  if (users.length > 0) redirect("/login");
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <Card>
-        <CardHeader className="text-center font-semibold">
-          Add Your First User
-        </CardHeader>
-        <CardContent className="space-y-4 w-96">
-          <Input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email..."
-          />
-
-          <Button
-            disabled={email === "" || loading}
-            onClick={submit}
-            className="w-full"
-          >
-            Add User
-          </Button>
-        </CardContent>
-      </Card>
+      <AddFirstUserForm />
     </div>
   );
 }
