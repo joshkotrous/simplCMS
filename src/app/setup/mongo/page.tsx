4c956734a0f524cade7de23cb8d64d8d5dc2c6ab
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2025-01-25 16:55:57 -0500

    Fixed mongo error

--- src/app/setup/mongo/page.tsx
+++ src/app/setup/mongo/page.tsx
@@ -3,19 +3,6 @@ import Link from "next/link";
 import SetupMongoForm from "./setupMongoDBForm";
 
 export default async function SetupMongo() {
-  if (!process.env.SIMPLCMS_HOST_PROVIDER) {
-    return (
-      <div className="size-full flex flex-col justify-center items-center gap-4 text-foreground">
-        <h2 className="font-semibold">
-          You must setup a host provider before connecting to a database.
-        </h2>
-        <Link href="/setup">
-          <Button>Setup Host Provider</Button>
-        </Link>
-      </div>
-    );
-  }
-
   return (
     <div className="size-full flex justify-center items-center text-foreground">
       <SetupMongoForm />
