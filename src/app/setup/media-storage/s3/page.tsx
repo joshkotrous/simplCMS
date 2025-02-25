ed9c99f6f8ffebc1d6558109b4751bb225057d47
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2025-02-25 03:18:20 -0500

    wip

--- src/app/setup/media-storage/s3/page.tsx
+++ src/app/setup/media-storage/s3/page.tsx
@@ -1,8 +1,10 @@
+import { getServerEnvVars } from "@/packages/core/src/simplCms";
 import SetupS3Form from "./setupS3Form";
 export default async function SetupS3Page() {
+  const platformConfiguration = getServerEnvVars();
   return (
     <div className="size-full flex justify-center items-center text-foreground">
-      <SetupS3Form />
+      <SetupS3Form platformConfiguration={platformConfiguration} />
     </div>
   );
 }
