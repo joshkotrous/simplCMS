71e3a8f12c08318246206de9fff5e7d58f586476
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2025-03-19 01:40:16 -0400

    Added canary workflow

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -14,12 +14,6 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "SimplCMS", // Usually your GitHub org/user name.
-  projectName: "Docs", // Usually your repo name.
-
   onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
